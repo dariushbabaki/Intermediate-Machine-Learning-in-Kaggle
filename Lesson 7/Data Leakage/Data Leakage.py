80d63d330ba4fea21c5b8e9526c2b594c114c501
Dariush Babaki <dariushbabaki1999@gmail.com>
2025-01-26 08:53:43 +0330

    Create Data Leakage.py

---
+++ Lesson 7/Data Leakage/Data Leakage.py
@@ -0,0 +1,36 @@
+# Set up code checking
+from learntools.core import binder
+binder.bind(globals())
+from learntools.ml_intermediate.ex7 import *
+print("Setup Complete")
+
+#Step 1: The Data Science of Shoelaces
+
+# Check your answer (Run this code cell to receive credit!)
+q_1.check()
+
+#Step 2: Return of the Shoelaces
+
+# Check your answer (Run this code cell to receive credit!)
+q_2.check()
+
+#Step 3: Getting Rich With Cryptocurrencies?
+
+# Check your answer (Run this code cell to receive credit!)
+q_3.check()
+
+#Step 4: Preventing Infections
+
+# Check your answer (Run this code cell to receive credit!)
+q_4.check()
+
+#Step 5: Housing Prices
+
+# Fill in the line below with one of 1, 2, 3 or 4.
+potential_leakage_feature = 2
+
+# Check your answer
+q_5.check()
+
+#q_5.hint()
+#q_5.solution()
